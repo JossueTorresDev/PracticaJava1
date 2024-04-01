dfa5ebbdf35548811d3b3eec4cf1fc397aa18786
JossueTorresDev <162776128+JossueTorresDev@users.noreply.github.com>
2024-04-01 08:59:25 -0500

    Add files via upload

---
+++ Personas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lab6
+ */
+public class Personas {
+    //Atributos
+    String nombre;
+    int edad;
+    int dni;
+    //Metodo
+    public void saludar(){
+        System.out.println("Hola");
+    }
+    
+    public void caminar(){
+        System.out.println("la persona esta caminando");
+    }
+    
+}
