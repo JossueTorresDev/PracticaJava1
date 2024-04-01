1a24324ff9b383e099e06bd9c281d6d96ff507a7
JossueTorresDev <162776128+JossueTorresDev@users.noreply.github.com>
2024-04-01 10:28:28 -0500

    Add files via upload

---
+++ Main_S05_17.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lab6
+ */
+public class Main_S05_17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora_17 obj1 = new Calculadora_17();
+        obj1.ingresarDatos();
+        obj1.restar();
+        obj1.sumar();
+        obj1.mostrarDatos();
+    }
+}   
+    /*
+    Personas persona1 = new Personas();
+    Personas persona2 = new Personas();
+    
+    persona1.altura = 1.85;
+    persona1.apellido = "Torres";
+    persona1.apodo = "El Menor";
+    persona1.dni = 87654321;
+    persona1.edad = 20;
+    persona1.nombre = "Jossue";
+    persona1.peso = 45.5;
+    persona1.saludar();
+    
+    }
+    
+}
+*/
